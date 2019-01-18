84fa3aa27d2686de3ae4b08f6825b7920eb83170
punqtured <casper@niebe.dk>
2019-01-18 14:25:19 +0100

    Rebranding
    Changed names, added Discord link and updated to new version of font-awesome (to include discord-icon)

--- www/footer.php
+++ www/footer.php
@@ -6,19 +6,22 @@
 <br><br>
 <a href="/">Home</a> | 
 <a href="./Top100Richest.php">Top 100 richest</a> | 
-<a href="./byteballworldmap.php">Byteball world map</a> | 
+<a href="./obyteworldmap.php">O<sub>byte</sub> world map</a> | 
 <a href="./witnesses.php">Witnesses</a> | 
-<a href="./heartbeat.php">Byteball HeartBeat</a>
+<a href="./heartbeat.php">O<sub>byte</sub> HeartBeat</a>
 </center>
 <br><br>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<center>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+	<center>
 New to Byteball? Check out <a href="https://byteball.org" target="_blank">byteball.org</a>
-&nbsp;also on Twitter <a href="https://twitter.com/ByteballOrg" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a>
-&nbsp;Bitcointalk <a href="https://bitcointalk.org/index.php?topic=1608859.0" target="_blank" title="BitcoinTalk thread"><i class="fa fa-bitcoin"></i></a>
-&nbsp;Medium <a href="https://medium.com/@Byteball" target="_blank" title="Medium"><i class="fa fa-medium"></i></a>
-&nbsp;Slack <a href="http://slack.byteball.org" target="_blank" title="Slack"><i class="fa fa-slack"></i></a>
-&nbsp;and Wiki <a href="https://wiki.byteball.org/" target="_blank" title="Wiki"><i class="fa fa-wikipedia-w"></i></a>
+&nbsp;also on Twitter <a href="https://twitter.com/ByteballOrg" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a>
+&nbsp;Bitcointalk <a href="https://bitcointalk.org/index.php?topic=1608859.0" target="_blank" title="BitcoinTalk thread"><i class="fab fa-bitcoin"></i></a>
+&nbsp;Medium <a href="https://medium.com/@Byteball" target="_blank" title="Medium"><i class="fab fa-medium"></i></a>
+&nbsp;Slack <a href="http://slack.byteball.org" target="_blank" title="Slack"><i class="fab fa-slack"></i></a>
+&nbsp;Wiki <a href="https://wiki.byteball.org/" target="_blank" title="Wiki"><i class="fab fa-wikipedia-w"></i></a>
+&nbsp;and Discord <a href="https://discord.gg/Qn6JWfT" target="_blank" title="Discord"><i class="fab fa-discord"></i></a>
+
 </center>
 </font>
 <br><br>
